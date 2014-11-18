bbcaf4de7ad9a6d00e3336341fef13528d96978d
Emanuel Correa Rivera <emanuel.correa216@gmail.com>
2014-11-18 13:13:30 -0400

    Encryption module

--- programahonor/Registro.aspx.vb
+++ programahonor/Registro.aspx.vb
@@ -1,12 +1,20 @@
 ﻿Imports System.Data.SqlClient
+Imports System.Security.Cryptography
+Imports System.IO
 
 Public Class Registro
     Inherits System.Web.UI.Page
 
+    Protected EncryptedText As String
+    Protected DecryptedText As String
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
+
     Protected Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        'MsgBox(Encrypt(Stu_Password.Text))
 
         Dim cnDB As New SqlClient.SqlConnection
         cnDB.ConnectionString = "Data Source=ING_ALM03;User ID=waph;Password=waph!admin;"
@@ -14,9 +22,9 @@ Public Class Registro
         command.CommandType = CommandType.StoredProcedure
         cnDB.Open()
 
+        Stu_Password.Text = Encrypt(Stu_Password.Text)
         command.Parameters.Add("@Stu_User", SqlDbType.VarChar).Value = Stu_User.Text
         command.Parameters.Add("@Stu_Password", SqlDbType.VarChar).Value = Stu_Password.Text
-
 
         Try
             command.ExecuteNonQuery()
@@ -30,4 +38,26 @@ Public Class Registro
         Response.Redirect("Aplicar.aspx")
 
     End Sub
+
+    Protected Function Encrypt(password As String) As String
+
+        Dim passwordHashSha1 As String = _
+               SimpleHash.ComputeHash(password, "SHA1", Nothing)
+        'Nothing es el salt
+        'Añadir el primary key de los estudiantes para salt para mejor proteccion
+
+        Return passwordHashSha1
+    End Function
+
+    ' Esta funcion sirve para cuando el estudiante quiere hacer login
+    Protected Function verifyHash(password As String) As Boolean
+        Dim passwordHashSha1 As String = _
+              SimpleHash.ComputeHash(password, "SHA1", Nothing)
+
+        Dim verification As Boolean =
+            SimpleHash.VerifyHash(password, "SHA1", passwordHashSha1)
+
+        Return verification
+    End Function
+
 End Class
