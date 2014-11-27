4745440f227f1fac96ec14a6e3f2fb91aa2dabd7
Gabriel E. Monsalve <gabriel.monsalve@upr.edu>
2014-11-27 14:59:08 -0400

    creacion de directorios
    directorios para guardar los ensayos y los certificados de IGS o AGP

--- programahonor/Estudiantes.Master.vb
+++ programahonor/Estudiantes.Master.vb
@@ -3,7 +3,6 @@ Public Class Estudiantes
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        'Dim Username As String = PreviousPage.CurrentUser
     End Sub
 
 End Class
