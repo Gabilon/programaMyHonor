ce89b7665af9fd259920463a94cab25a5bc84cc2
carlosc13 <carlos.caban@upr.edu>
2014-11-29 15:06:06 -0400

    xmlestadodocente, cambio a contacto

--- programahonor/Contacto.aspx.vb
+++ programahonor/Contacto.aspx.vb
@@ -17,8 +17,8 @@ Public Class Contacto
             Try
                 Dim Smtp_Server As New SmtpClient
                 Dim e_mail As New MailMessage()
-                Smtp_Server.UseDefaultCredentials = True
-                Smtp_Server.Credentials = New Net.NetworkCredential("gerencia", "441121837")
+                'Smtp_Server.UseDefaultCredentials = True
+                Smtp_Server.Credentials = New Net.NetworkCredential("grupogerenciaccg@gmail.com", "gerencia02")
                 Smtp_Server.Port = 587
                 Smtp_Server.EnableSsl = True
                 Smtp_Server.Host = "smtp.gmail.com"
@@ -28,7 +28,7 @@ Public Class Contacto
                 e_mail.To.Add(New MailAddress("caban@live.com"))
                 e_mail.Subject = "Mensaje de Contacto Programa de Honor UPRB"
                 e_mail.IsBodyHtml = False
-                e_mail.Body = Contac_nombre.Text ', Contac_mensaje.Text
+                e_mail.Body = "Nombre: " & Contac_nombre.Text & vbNewLine & "Email: " & Contac_email.Text & vbNewLine & "Mensaje: " & Contac_mensaje.Text
                 Smtp_Server.Send(e_mail)
 
                 Response.Write("<script>alert('Email Enviado Satisfactoriamente'); </script>")
@@ -40,6 +40,7 @@ Public Class Contacto
         Else
             lblResult.Text = "El código ingresado esta incorrecto. Intente de nuevo."
             lblResult.ForeColor = Drawing.Color.Red
+            '<!--Hay que hacer que vuelva a entrar a la pregunta-->
         End If
 
     End Sub
